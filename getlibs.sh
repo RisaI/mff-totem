041f8023a97c829e82f0a95c0ac4135ae840fc08
Richard Ivanek <richard.ivanek@gmail.com>
2017-12-06 00:03:14 +0100

    Fixed content naming and getlibs script

--- getlibs.sh
+++ getlibs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
 
-#curl http://fna.flibitijibibo.com/archive/fnalibs.tar.bz2 > libs.tar.bz2
+curl http://fna.flibitijibibo.com/archive/fnalibs.tar.bz2 > libs.tar.bz2
 mkdir libs
 tar xf libs.tar.bz2 -C libs
 rm libs.tar.bz2
+echo Libs have been extracted to the libs/ directory. Copy files for your platform to the debug folder.
