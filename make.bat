26fd4bc54ae2c3df00e9c6e4a710428c78730822
Michal Grňo <m93a.cz@gmail.com>
2018-02-18 01:02:50 +0100

    Somewhat buggy make.bat implementation

--- make.bat
+++ make.bat
@@ -11,3 +11,32 @@ if "%1" == "build" (
 
     %msbuild%
 )
+
+if "%1" == "content" (
+
+    cd Content;
+    mgcb Content.mgcb
+    .\shaders.sh
+    cd ..
+    mkdir Mff.Totembin\DesktopGL\AnyCPU\Debug\Content
+    copy /Y Content\bin\DesktopGL\* Mff.Totem\bin\DesktopGL\AnyCPU\Debug\Content\
+
+)
+
+if "%1" == "content-ns" (
+
+    cd Content;
+    mgcb /build:Content.mgcb
+    cd ..
+    mkdir Mff.Totembin\DesktopGL\AnyCPU\Debug\Content
+    copy /Y Content\bin\DesktopGL\* Mff.Totem\bin\DesktopGL\AnyCPU\Debug\Content\
+
+)
+
+if "%1" = "run" (
+    Mff.Totem/bin/DesktopGL/AnyCPU/Debug//Mff.Totem.exe
+)
+
+if "%1" = "clean" (
+    del /Q Mff.Totem/bin/*
+)
